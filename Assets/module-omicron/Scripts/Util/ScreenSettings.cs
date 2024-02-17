813513e9db4dcf912aeaf35763fbebb124feeebe
Manan Jain <mjain35@uic.edu>
2024-02-16 18:10:36 -0600

    Utils file

---
+++ Assets/module-omicron/Scripts/Util/ScreenSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSettings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(3840, 2160, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Screen.SetResolution(3840, 2160, true);
+    }
+}
